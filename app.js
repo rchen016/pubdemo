ed9037b7a4d9f67f9286086e7fff04c56b5680be
Chen <rchen@CompanyNet.org>
2019-04-25 15:43:18 -0700

    added most of the base of homepage and start of gpt

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var express          = require("express"),
-    app              = express();
-
+var express = require("express"),
+    app     = express(),
+	path    = require("path");
+	Swiper  = require("swiper")
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine","ejs");
 app.get("/",function(req,res){
 	res.render("homepage");
