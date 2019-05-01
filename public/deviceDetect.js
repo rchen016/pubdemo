483eca8b157b7928e07dd645f23629f31966003b
Chen <rchen@CompanyNet.org>
2019-04-30 17:00:56 -0700

    added viewport logic and mobile ads

---
+++ public/deviceDetect.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	var height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+	console.log("width: ", width);
+	console.log("height: ", height);
+});
